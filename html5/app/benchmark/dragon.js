f13f6dc84c633599cd144d53960309f2b303596a
yejingfu <jingfu.ye@intel.com>
2014-11-03 14:05:10 +0800

    add dragon animation

--- html5/app/benchmark/dragon.js
+++ html5/app/benchmark/dragon.js
@@ -5,6 +5,7 @@ define(function(){
     this.canWidth = 0;
     this.canHeight = 0;
     this.imgWidth = 1286;
+    this.imgHeight = 640;
     this.assetLoader = null;
     this.background = null;
     this.background2 = null;
@@ -12,6 +13,7 @@ define(function(){
     this.foreground = null;
     this.foreground2 = null;
     this.foreground3 = null;
+    this.dragon = null;
     this.ntf = null;
     this.started = false;
   };
@@ -23,7 +25,8 @@ define(function(){
       this.ntf = ctx.notifications;
       this.canWidth = this.renderer.view.width;
       this.canHeight = this.renderer.view.height;
-      this.assetLoader = new PIXI.AssetLoader(['data/bgtile.jpg', 'data/ground.png']);
+      this.assetLoader = new PIXI.AssetLoader(['data/bgtile.jpg', 'data/ground.png', 
+        'data/dragonbones.json', 'data/dragonbones.anim']);
       var self = this;
       //this.assetLoader.onComplete = function() {
       this.assetLoader.on('onComplete', function() {
@@ -33,6 +36,13 @@ define(function(){
         self.foreground = PIXI.Sprite.fromImage('data/ground.png');
         self.foreground2 = PIXI.Sprite.fromImage('data/ground.png');
         self.foreground3 = PIXI.Sprite.fromImage('data/ground.png');
+
+        // dragon
+        self.dragon = new PIXI.Spine('data/dragonbones.anim');
+        self.dragon.position.x = self.imgWidth / 3;
+        self.dragon.position.y = self.imgHeight / 2 + 80;
+        self.dragon.scale.x = self.dragon.scale.y = 0.5;
+        self.dragon.state.setAnimationByName('flying', true);
       });
       this.assetLoader.load();
     },
@@ -52,6 +62,8 @@ define(function(){
         self.foreground.position.y = 
         self.foreground2.position.y = 
         self.foreground3.position.y = 640 - self.foreground2.height;
+
+        self.stage.addChild(self.dragon);
 
         self.ntf.update.add(function() {
           self.onUpdate();
@@ -94,7 +106,7 @@ define(function(){
           this.background.position.x = 0;
         }
         if (this.stage.children.indexOf(this.foreground3) === -1) {
-          this.stage.addChild(this.foreground3);
+          this.stage.addChildAt(this.foreground3, this.stage.getChildIndex(this.foreground2));
           this.foreground3.position.y = 640 - this.foreground3.height;
           this.foreground.position.x = 0;
         }
